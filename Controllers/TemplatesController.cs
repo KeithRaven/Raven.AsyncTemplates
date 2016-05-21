d62e0a58ac7a82c536d30dec9164f9099256bd86
Keith Raven <keith@keth.co.uk>
2016-05-21 23:40:19 +0100

    updated to reflect changes in api module

--- Controllers/TemplatesController.cs
+++ Controllers/TemplatesController.cs
@@ -6,23 +6,32 @@ using System.Web.Mvc;
 using Orchard;
 using Orchard.DisplayManagement.Descriptors;
 using Orchard.Mvc;
+using Raven.Api;
 
 namespace Raven.AsyncTemplates.Controllers
 {
     public class TemplatesController : Controller
     {
         private readonly IOrchardServices _services;
+        private readonly IBindingTypeCreateAlterations _alterations;
 
         public TemplatesController(
-            IOrchardServices services) {
+            IOrchardServices services,
+            IBindingTypeCreateAlterations alterations) {
             _services = services;
+            _alterations = alterations;
         }
 
         // GET: Templates
         public ActionResult Index(string contentType, string templateType ,string displayType = "Summary")
         {
             var model = _services.ContentManager.New(contentType);
-            var vm = _services.ContentManager.BuildDisplay(model, displayType, bindingType: templateType);
+
+            dynamic vm;
+            using (_alterations.CreateScope(templateType)) {
+                vm = _services.ContentManager.BuildDisplay(model, displayType);
+            }
+           
             return View(vm);
         }
     }
